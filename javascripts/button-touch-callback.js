7350fdb7816594c13bb197681021c10a284eca50
cacheflowe <cacheflowe@cacheflowe.com>
2013-06-17 18:49:53 -0600

    Fix: add .tts namespace

--- javascripts/button-touch-callback.js
+++ javascripts/button-touch-callback.js
@@ -8,24 +8,24 @@ function ButtonTouchCallback( element, callback, highlightClass ) {
   this.canceled = false;
   // create touch tracker
   var self = this;
-  this.touch_tracker = new MouseAndTouchTracker( this.element, function( touchState, touchEvent ) {
+  this.touch_tracker = new tts.MouseAndTouchTracker( this.element, function( touchState, touchEvent ) {
     self.touchUpdated( touchState, touchEvent );
   }, false, '' );
 }
 
 ButtonTouchCallback.prototype.touchUpdated = function ( touchState, touchEvent ) {
-  if( touchState == MouseAndTouchTracker.state_start ) {
+  if( touchState == tts.MouseAndTouchTracker.state_start ) {
     if( this.highlight_class ) this.element.className = [this.element.className, this.highlight_class].join(' ');
     this.started_touching = true;
     this.canceled = false;
   }
   // cancel click if mouse/touch moves past threshold
-  if( touchState == MouseAndTouchTracker.state_move ) {
+  if( touchState == tts.MouseAndTouchTracker.state_move ) {
     if( Math.abs( this.touch_tracker.touchmoved.x ) + Math.abs( this.touch_tracker.touchmoved.y ) >= this.CANCEL_THRESHOLD ) this.canceled = true;
     if( Math.abs( this.touch_tracker.touchspeed.x ) + Math.abs( this.touch_tracker.touchspeed.y ) >= this.CANCEL_THRESHOLD ) this.canceled = true;
     if( this.canceled && this.highlight_class ) this.element.className = this.element.className.replace(this.highlight_class, '');
   }
-  if( touchState == MouseAndTouchTracker.state_end ) {
+  if( touchState == tts.MouseAndTouchTracker.state_end ) {
     if( this.highlight_class ) this.element.className = this.element.className.replace(this.highlight_class, '');
     // call callback method if touch didn't move past threshold
     var moveTotal = Math.abs( this.touch_tracker.touchmoved.x ) + Math.abs( this.touch_tracker.touchmoved.y );
